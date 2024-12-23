5529bf5e7b02013e337091327783eab781f894af
Darrin Allen <darrin.lovard.allen@gmail.com>
2024-12-22 17:35:03 -0800

    Create two-num-sum2.js

---
+++ Easy/two-num-sum2.js
@@ -0,0 +1,15 @@
+
+   function twoNumberSum(array, targetSum) {
+   // Write your code here.
+    const nums ={};
+        for (const num of array){
+            const potentialMatch = targetSum - num;
+            if (potentialMatch in nums) {
+                return [potentialMatch, num]
+            }
+            else {
+                nums[num] = true;
+            }
+        }
+        return [];
+    }
